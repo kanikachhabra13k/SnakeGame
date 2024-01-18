9f875fc9737cb2ca11d276c0234459c65d8fa129
KANIKA CHHABRA <114740561+kanikachhabra13k@users.noreply.github.com>
2024-01-18 22:22:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,19 +42,6 @@ while game_is_on:
 
 
 
-# class Animal:
-#     def __init__(self):
-#         self.num_eyes = 2
-#     def breathe(self):
-#         print("Inhale, exhale.")
-# class Fish(Animal):
-#     def __init__(self):
-#         super().__init__()
-#     def breathe(self):
-#         super().breathe()
-#         print("doing this underwater.")
-#     def swim(self):
-#         print("moving in water.")
 
 
 
